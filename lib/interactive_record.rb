c80cb9fa3fab76718063e941a4c777b1198d8815
John Hayes <johnnyhayes7@gmail.com>
2019-03-29 22:21:11 +0000

    Automatically backed up by Learn

--- lib/interactive_record.rb
+++ lib/interactive_record.rb
@@ -58,8 +58,8 @@ class InteractiveRecord
   def self.find_by(kv_pair)
     key = kv_pair.keys.join() 
     value = kv_pair.values.first
-    sql = "SELECT * FROM #{self.table_name} WHERE ? = ?"
-    DB[:conn].execute(sql, key, value)
+    sql = "SELECT * FROM #{self.table_name} WHERE #{key} = #{value}"
+    DB[:conn].execute(sql)
   end
   
   
