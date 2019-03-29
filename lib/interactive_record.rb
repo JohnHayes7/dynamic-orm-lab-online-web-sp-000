828fbfd7f939bf6ec889bcd683c4b831d2841194
John Hayes <johnnyhayes7@gmail.com>
2019-03-29 22:14:25 +0000

    Automatically backed up by Learn

--- lib/interactive_record.rb
+++ lib/interactive_record.rb
@@ -56,7 +56,7 @@ class InteractiveRecord
   end
   
   def self.find_by(kv_pair)
-    key = kv_pair.keys.
+    key = kv_pair.keys.first
     binding.pry
       sql = "SELECT * FROM #{self.table_name} WHERE ? = ?"
       DB[:conn].execute(sql)
